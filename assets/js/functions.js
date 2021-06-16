0280ceed9870d7cf307d4c8275124a969b353bd3
Luis Fernando <82784821+LuisFsJr@users.noreply.github.com>
2021-06-16 11:26:58 -0300

    adicionando funcionalidades do menu mobile

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,9 @@
 $(function(){
 
-
+    /* Funcionalidade do Menu Mobile */
+    $('nav.mobile i').click(function(){
+        $(this).parent().find('ul').animate({
+            width: "toggle"
+        })
+    })
 })
